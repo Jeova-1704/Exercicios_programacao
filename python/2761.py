9f98158bc88189996c8e1ceaf92e924cf9bec161
Jeova BL <jeovab115@gmail.com>
2023-05-11 21:28:47 -0300

    mais 3 exercicios

---
+++ python/2761.py
@@ -0,0 +1,9 @@
+n = int(input())
+x = float(input())
+y = str(input())
+z = str(input())
+
+print(f'{n}{x}{y}{z}')
+print(f'{n}\t{x}\t{y}\t{z}')
+print(f'{n:<10}{x:<10}{y:<10}{z:<10}')
+
---
+++ python/2766.py
@@ -0,0 +1,6 @@
+nomes = list()
+for x in range(10):
+    nomes.append(input().strip())
+print(nomes[2])
+print(nomes[6])
+print(nomes[8])
