9ada8d6250f75de0e1423aca03b52698ac2311b9
Jeova BL <jeovab115@gmail.com>
2023-06-18 19:06:24 -0300

    solução de mais 2 exercicios

--- python/beecrowd/1023.py
+++ python/beecrowd/1023.py
@@ -37,32 +37,32 @@
 #     consumo_por_pessoa.append([consumo_pessoal[0], consumo])
 
 
-cidade = 1
-while True:
-    casos = int(input())
-    if casos == 0:
-        break
+# cidade = 1
+# while True:
+#     casos = int(input())
+#     if casos == 0:
+#         break
 
-    dados_pessoas = []
-    consumo_por_pessoa = []
-    consumo_maximo = 0
-    total_pessoas = 0
-    for _ in range(casos):
-        x, y = map(int, input().split())
-        consumo_maximo += y
-        total_pessoas += x
-        dados_pessoas.append([x, y])
+#     dados_pessoas = []
+#     consumo_por_pessoa = []
+#     consumo_maximo = 0
+#     total_pessoas = 0
+#     for _ in range(casos):
+#         x, y = map(int, input().split())
+#         consumo_maximo += y
+#         total_pessoas += x
+#         dados_pessoas.append([x, y])
     
-    consumo_por_pessoa = [[dados[0], dados[1] // dados[0]] for dados in dados_pessoas]
+#     consumo_por_pessoa = [[dados[0], dados[1] // dados[0]] for dados in dados_pessoas]
 
-    consumo_medio = consumo_maximo / total_pessoas
-    consumo_por_pessoa.sort(key=lambda x: x[1])
+#     consumo_medio = consumo_maximo / total_pessoas
+#     consumo_por_pessoa.sort(key=lambda x: x[1])
 
-    print(f'Cidade# {cidade}:')
-    for dados in consumo_por_pessoa:
-        print(f'{dados[0]}-{dados[1]}', end=' ')
-    print(f'\nConsumo medio: {consumo_medio:.2f} m3.\n')
-    cidade += 1
+#     print(f'Cidade# {cidade}:')
+#     for dados in consumo_por_pessoa:
+#         print(f'{dados[0]}-{dados[1]}', end=' ')
+#     print(f'\nConsumo medio: {consumo_medio:.2f} m3.\n')
+#     cidade += 1
 
 
 
@@ -155,7 +155,6 @@ while True:
 
 
 # cidade = 1
-
 # while True:
 #     casos = int(input())
     
@@ -173,9 +172,175 @@ while True:
 #         consumo_por_pessoa.append([x, y // x])
     
 #     consumo_medio = consumo_maximo / total_pessoas
+#     consumo_por_pessoa.sort(key=lambda x: x[1])
 
 #     print(f'Cidade# {cidade}:')
 #     for x, consumo in consumo_por_pessoa:
 #         print(f'{x}-{consumo}', end=' ')
 #     print(f'\nConsumo medio: {consumo_medio:.2f} m3.\n')
 #     cidade += 1
+
+
+
+# cidade = 1
+
+# while True:
+#     casos = int(input())
+#     if casos == 0:
+#         break
+    
+#     consumo = {}
+#     consumo_total = 0
+#     total_residentes = 0
+    
+#     for _ in range(casos):
+#         residentes, valor_consumo = map(int, input().split())
+        
+#         chave = valor_consumo // residentes
+#         if chave in consumo:
+#             consumo[chave] += residentes
+#         else:
+#             consumo[chave] = residentes
+#         consumo_total += valor_consumo
+#         total_residentes += residentes
+    
+#     print(f"Cidade# {cidade}:")
+    
+#     for consumo_pessoa, residentes in sorted(consumo.items()):
+#         print(f"{residentes}-{consumo_pessoa}", end=" ")
+    
+#     media = consumo_total / total_residentes
+#     print("\nConsumo medio: {:.2f} m3.".format(media))
+#     print()
+#     cidade += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # erro de 100%
+
+# cidade = 1
+
+# while True:
+#     casos = int(input())
+#     if casos == 0:
+#         break
+    
+#     consumo = {}
+#     consumo_total = 0
+#     total_residentes = 0
+    
+#     for _ in range(casos):
+#         residentes, valor_consumo = map(int, input().split())
+        
+#         chave = valor_consumo // residentes
+#         if chave in consumo:
+#             consumo[chave] += residentes
+#         else:
+#             consumo[chave] = residentes
+#         consumo_total += valor_consumo
+#         total_residentes += residentes
+    
+#     print(f"Cidade# {cidade}:")
+    
+#     for consumo_pessoa, residentes in sorted(consumo.items()):
+#         print(f"{residentes}-{consumo_pessoa}", end=" ")
+    
+#     media = consumo_total / total_residentes
+#     print(f"\nConsumo medio: {media:.2f} m3.")
+#     print()
+#     cidade += 1
+
+
+
+
+
+
+# def main():
+#     from sys import stdin, stdout
+#     from collections import defaultdict
+#     from operator import itemgetter
+#     from math import floor
+
+#     def truncate(f):
+#         return floor(f * 100) / 100
+
+#     cidade = 1
+#     casos = int(stdin.readline())
+#     while True:
+#         consumptions = defaultdict(int)
+#         total_consumption = total_residents = 0
+#         for _ in range(casos):
+#             x, y = map(int, stdin.readline().split())
+#             consumptions[y // x] += x
+#             total_consumption += y
+#             total_residents += x
+
+#         stdout.write(f'Cidade# {cidade}:\n')
+#         stdout.write(' '.join(f'{c[1]}-{c[0]}' for c in sorted(consumptions.items(), key=itemgetter(0))))
+#         stdout.write(f'\nConsumo medio: {truncate(total_consumption / total_residents):.2f} m3.\n')
+
+#         casos = int(stdin.readline())
+#         if casos == 0:
+#             break
+#         stdout.write('\n')
+#         cidade += 1
+
+# main()
+
+
+
+import math
+
+T = 0
+while True:
+    try:
+        N = int(input())
+
+        if(N == 0):
+            break
+
+        if(T):
+            print('')
+
+        totalX, totalY = 0, 0
+        consumos = {}
+        for _ in range(N):
+            X, Y = map(int, input().split(' '))
+
+            totalX += X
+            totalY += Y
+
+            if (Y//X in consumos):
+                consumos[Y//X] += X
+            else:
+                consumos[Y//X] = X
+
+        consumo_total = ((100 * totalY)/totalX)/100
+
+        T += 1
+        print(f'Cidade# {T}:')
+
+        output = []
+        keys = sorted(list(consumos.keys()))
+        for key in keys:
+            output.append(f'{consumos[key]}-{key}')
+
+        print(f'{" ".join(output)}')
+
+        consumo_medio = math.floor((100 * totalY)/totalX)/100
+
+        print(f'Consumo medio: {consumo_medio:.2f} m3.')
+    except EOFError:
+        break
--- python/beecrowd/1025.py
+++ python/beecrowd/1025.py
@@ -1,15 +1,15 @@
-"""
-Raju e Meena adoram jogar um jogo diferente com pequenas peças de mármores, chamados Marbles. Eles têm um monte destas peças com números escritos neles. No início, Raju colocaria estes pequenos mármores um após outro em ordem ascendente de números escritos neles. Então Meena gostaria de pedir a Raju para encontrar o primeiro mármore com um certo número. Ele deveria contar 1...2...3. Raju ganha um ponto por cada resposta correta e Meena ganha um ponto se Raju falha. Depois de um número fixo de tentativas, o jogo termina e o jogador com o máximo de pontos vence. Hoje é sua chance de jogar com Raju. Sendo um/a cara esperto/a, você tem em seu favor o computador. Mas não subestime Meena, ela escreveu um programa para monitorar quanto tempo você levará para dar todas as respostas. Portanto, agora escreva o programa, que ajudará você em seu desafio com Raju.
+# """
+# Raju e Meena adoram jogar um jogo diferente com pequenas peças de mármores, chamados Marbles. Eles têm um monte destas peças com números escritos neles. No início, Raju colocaria estes pequenos mármores um após outro em ordem ascendente de números escritos neles. Então Meena gostaria de pedir a Raju para encontrar o primeiro mármore com um certo número. Ele deveria contar 1...2...3. Raju ganha um ponto por cada resposta correta e Meena ganha um ponto se Raju falha. Depois de um número fixo de tentativas, o jogo termina e o jogador com o máximo de pontos vence. Hoje é sua chance de jogar com Raju. Sendo um/a cara esperto/a, você tem em seu favor o computador. Mas não subestime Meena, ela escreveu um programa para monitorar quanto tempo você levará para dar todas as respostas. Portanto, agora escreva o programa, que ajudará você em seu desafio com Raju.
 
-Entrada
-A entrada contém vários casos de teste, mas o total de casos é menor do que 65. Cada caso de teste inicia com dois inteiros: N que é o número de mármores e Q que é o número de consultas que Meena deseja fazer. As próximas N linhas conterão os números escritos em cada um dos N mármores. Os números destes mármores não tem qualquer ordem em particular. As seguintes Q linhas irão conter Q consultas. Tenha certeza, nenhum dos números da entrada é maior do que 10000 e nenhum deles é negativo.
-A entrada é terminada por um caso de teste onde N = 0 e Q = 0.
+# Entrada
+# A entrada contém vários casos de teste, mas o total de casos é menor do que 65. Cada caso de teste inicia com dois inteiros: N que é o número de mármores e Q que é o número de consultas que Meena deseja fazer. As próximas N linhas conterão os números escritos em cada um dos N mármores. Os números destes mármores não tem qualquer ordem em particular. As seguintes Q linhas irão conter Q consultas. Tenha certeza, nenhum dos números da entrada é maior do que 10000 e nenhum deles é negativo.
+# A entrada é terminada por um caso de teste onde N = 0 e Q = 0.
 
-Saída
-Para cada caso de teste de saída deve haver um número serial do caso de teste. Para cada consulta, escreva uma linha de saída. O formato desta linha dependerá se o número consultado estiver ou não escrito em um dos mármores. Os dois diferentes formatos são descritos abaixo:
-'x found at y', se o primeiro marble x foi encontrado na posição y. Posições são numeradas de 1, 2,...  a N.
-'x not found', se o marble com o número x não estiver presente.
-"""
+# Saída
+# Para cada caso de teste de saída deve haver um número serial do caso de teste. Para cada consulta, escreva uma linha de saída. O formato desta linha dependerá se o número consultado estiver ou não escrito em um dos mármores. Os dois diferentes formatos são descritos abaixo:
+# 'x found at y', se o primeiro marble x foi encontrado na posição y. Posições são numeradas de 1, 2,...  a N.
+# 'x not found', se o marble com o número x não estiver presente.
+# """
 # contador = 1
 # while contador <= 65:
 #     numeros_marmore = []
@@ -40,25 +40,29 @@ Para cada caso de teste de saída deve haver um número serial do caso de teste.
 #     contador += 1
 
 
+T = 1
+while True:
+    try:
+        N, Q = [int(x) for x in input().strip().split(' ')]
 
-caso = 1
-while caso <= 60:
-    marmore = []
-    N, Q = map(int, input().split())
-    if N == Q == 0:
+        if(N == 0 and Q == 0):
+            break
+
+        marmores = [0 for _ in range(10000)]
+        for _ in range(N):
+            marmores[int(input())] += 1
+
+        for i in range(1, 10000):
+            marmores[i] += marmores[i - 1]
+
+        print(f'CASE# {T}:')
+        T += 1
+        for _ in range(Q):
+            x = int(input())
+
+            if(marmores[x] == marmores[x - 1]):
+                print(f'{x} not found')
+            else:
+                print(f'{x} found at {marmores[x - 1] + 1}')
+    except EOFError:
         break
-
-    for _ in range(N):
-        marmore.append(int(input()))
-
-    marmore.sort()
-
-    print(f'CASE# {caso}:')
-    for _ in range(Q):
-        x = int(input())
-        if x in marmore:
-            print(f'{x} found at {marmore.index(x)}')
-        else:
-            print(f'{x} not found')
-            
-    caso += 1
